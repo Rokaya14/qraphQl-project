a27f9ff69a0fe48cf41becd12b44f0dfd8aa11a6
Rokaya <rokaiamagdi@gmail.com>
2021-07-05 00:23:54 +0200

    create type relation between autors & books

--- Server/schema/schema.js
+++ Server/schema/schema.js
@@ -1,9 +1,10 @@
 const _ = require("lodash");
 const graphql = require("graphql");
 let books = [
-  { name: "Name of the wind", genre: "fantasy", id: "1" },
-  { name: "the final empire", genre: "since fiction", id: "2" },
-  { name: "the long earth", genre: "Sci-fi", id: "3" },
+  { name: "Name of the wind", genre: "fantasy", id: "1" ,authorid:"1"},
+  { name: "the final empire", genre: "since fiction", id: "2" ,authorid:"2"},
+  { name: "the long earth", genre: "Sci-fi", id: "3" ,authorid:"3"},
+
 ];
 let authors = [
   { name: "patrick rothfuss", age: "44", id: "1" },
@@ -16,6 +17,13 @@ const BookType = new graphql.GraphQLObjectType({
     id: { type: graphql.GraphQLID },
     name: { type: graphql.GraphQLString },
     genre: { type: graphql.GraphQLString },
+
+    author:{
+      type:AuthorType,
+      resolve(parent, args){
+        return _.find(authors , {id:parent.authorid})
+      }
+    }
   }),
 });
 const AuthorType = new graphql.GraphQLObjectType({
