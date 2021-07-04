9d0b750111e8c316c57c7271ceb15ba9bbd57e0d
Rokaya <rokaiamagdi@gmail.com>
2021-07-04 23:38:40 +0200

    create new type >> Author

--- Server/schema/schema.js
+++ Server/schema/schema.js
@@ -1,33 +1,50 @@
-
-const _=require("lodash")
-const graphql =require("graphql") ;
- let books = [
-     {name: "Name of the wind", genre :"fantasy", id:"1"},
-     {name: "Harry poter", genre :"since fiction", id:"2"},
-     {name: "the long earth", genre :"Sci-fi",id:"3"}
- ]
-
+const _ = require("lodash");
+const graphql = require("graphql");
+let books = [
+  { name: "Name of the wind", genre: "fantasy", id: "1" },
+  { name: "the final empire", genre: "since fiction", id: "2" },
+  { name: "the long earth", genre: "Sci-fi", id: "3" },
+];
+let authors = [
+  { name: "patrick rothfuss", age: "44", id: "1" },
+  { name: "Brandon Sanderson", age: "42", id: "2" },
+  { name: "Terry Pratcchett", age: "66", id: "3" },
+];
 const BookType = new graphql.GraphQLObjectType({
-    name:"Book",
-    fields:()=>({
-        id:{type:graphql.GraphQLID},
-        name:{type:graphql.GraphQLString},
-        genre:{type:graphql.GraphQLString}
-    })
+  name: "Book",
+  fields: () => ({
+    id: { type: graphql.GraphQLID },
+    name: { type: graphql.GraphQLString },
+    genre: { type: graphql.GraphQLString },
+  }),
 });
-
+const AuthorType = new graphql.GraphQLObjectType({
+  name: "Author",
+  fields: () => ({
+    id: { type: graphql.GraphQLID },
+    name: { type: graphql.GraphQLString },
+    age: { type: graphql.GraphQLInt },
+  }),
+});
 const RootQuery = new graphql.GraphQLObjectType({
-    name :'RootQueryType',
-    fields:{
-        book:{
-            type:BookType,
-            args:{id:{type:graphql.GraphQLID}},
-           resolve(parent,args){
-            return _.find(books,{id: args.id})
-           }
-        }
-    }
-})
-let schema = new graphql.GraphQLSchema({query: RootQuery})
+  name: "RootQueryType",
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: graphql.GraphQLID } },
+      resolve(parent, args) {
+        return _.find(books, { id: args.id });
+      },
+    },
+    author: {
+      type: AuthorType,
+      args: { id: { type: graphql.GraphQLID } },
+      resolve(parent, args) {
+        return _.find(authors, { id: args.id });
+      },
+    },
+  },
+});
+let schema = new graphql.GraphQLSchema({ query: RootQuery });
 
-module.exports = schema
+module.exports = schema;
