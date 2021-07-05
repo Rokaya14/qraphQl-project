d948e71281d8b291209e47e8c3f83e5e7739dc4c
Rokaya <rokaiamagdi@gmail.com>
2021-07-05 22:36:00 +0200

    use es6 function

--- Server/schema/schema.js
+++ Server/schema/schema.js
@@ -1,9 +1,9 @@
-const _ = require("lodash");
 const graphql = require("graphql");
 let books = [
   { name: "Name of the wind", genre: "fantasy", id: "1" ,authorid:"1"},
   { name: "the final empire", genre: "since fiction", id: "2" ,authorid:"2"},
   { name: "the long earth", genre: "Sci-fi", id: "3" ,authorid:"3"},
+  { name: "the Hero of Ages", genre: "fantasy", id: "4" ,authorid:"2"},
 
 ];
 let authors = [
@@ -17,11 +17,12 @@ const BookType = new graphql.GraphQLObjectType({
     id: { type: graphql.GraphQLID },
     name: { type: graphql.GraphQLString },
     genre: { type: graphql.GraphQLString },
-
+    //relation between book and his author
     author:{
       type:AuthorType,
       resolve(parent, args){
-        return _.find(authors , {id:parent.authorid})
+        // console.log(parent);
+        return authors.find( author=>author.id===parent.authorid)
       }
     }
   }),
@@ -32,6 +33,13 @@ const AuthorType = new graphql.GraphQLObjectType({
     id: { type: graphql.GraphQLID },
     name: { type: graphql.GraphQLString },
     age: { type: graphql.GraphQLInt },
+    //relation between author and his books  
+    book:{
+      type : graphql.GraphQLList(BookType),
+      resolve(parent,args){
+        return books.filter(book=> book.authorid=== parent.id )
+      }
+    }
   }),
 });
 const RootQuery = new graphql.GraphQLObjectType({
@@ -41,14 +49,14 @@ const RootQuery = new graphql.GraphQLObjectType({
       type: BookType,
       args: { id: { type: graphql.GraphQLID } },
       resolve(parent, args) {
-        return _.find(books, { id: args.id });
+        return books.find(book => book.id === args.id );
       },
     },
     author: {
       type: AuthorType,
       args: { id: { type: graphql.GraphQLID } },
       resolve(parent, args) {
-        return _.find(authors, { id: args.id });
+        return authors.find(author=>  author.id ===args.id );
       },
     },
   },
