022bda1a11070467f96d15c05951c3c93aa878fd
Rokaya <rokaiamagdi@gmail.com>
2021-07-08 10:03:36 +0200

    create BookList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
-
+import BookList from './components/BookList';
 function App() {
   return (
     <div className="App">
       <h1>react App</h1>
+      <BookList/>
     </div>
   );
 }
---
+++ client/src/components/BookList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const BoolList = () => {
+    return ( 
+        <div id="book-list">
+                <li>Book name</li>
+            </div>
+     );
+}
+ 
+export default BoolList;
