cafddf9f9bb113f7e818d331be6a838b6eabcb3a
Rokaya <rokaiamagdi@gmail.com>
2021-07-08 13:51:18 +0200

    connect to graphql

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,10 @@
 import BookList from "./components/BookList";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  useQuery,
-  gql,
-} from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import AddBook from "./components/AddBook";
 
 //Apollo cleint setup
 const client = new ApolloClient({
-  uri: "https://http://localhost:4000/graphql", //will make request to this endpoint "GraphQL server.".
+  uri: "http://localhost:4000/graphql", //will make request to this endpoint "GraphQL server.".
   cache: new InMemoryCache(),
 });
 
@@ -17,8 +12,10 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <div className="App">
-        <h1>react App</h1>
-        <BookList />
+        <h1>Reading List </h1>
+        
+          <BookList />
+          <AddBook/>
       </div>
     </ApolloProvider>
   );
--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,10 +1,21 @@
 import React from "react";
-const BoolList = () => {
-  return (
-    <div id="book-list">
-      <li>Book name</li>
-    </div>
-  );
+import { graphql } from "@apollo/client/react/hoc";
+import {getBooksQuery} from '../Queries/queries'
+ 
+
+const BoolList = (props) => {
+  const displayBooks = () => {
+    let data = props.data;
+    if (data.loading) {
+      return <div>Loading Books</div>;
+    } else {
+      return data.books.map((book) => {
+        return <li key={book.id}>{book.name}</li>;
+      });
+    }
+  };
+
+  return <div id="book-list">{displayBooks()}</div>;
 };
 
-export default BoolList;
+export default graphql(getBooksQuery)(BoolList);
