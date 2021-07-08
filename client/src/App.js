7cd680d9beca6ce239722d374df95486ce695487
Rokaya <rokaiamagdi@gmail.com>
2021-07-08 10:21:20 +0200

    setup Apollo graphQl client

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
-import BookList from './components/BookList';
+import BookList from "./components/BookList";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
+
+//Apollo cleint setup
+const client = new ApolloClient({
+  uri: "https://http://localhost:4000/graphql", //will make request to this endpoint "GraphQL server.".
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <div className="App">
-      <h1>react App</h1>
-      <BookList/>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1>react App</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 }
 
