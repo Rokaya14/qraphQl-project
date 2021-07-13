91dd87f02804aa3b454cc505d27cc9ed08cf13dc
Rokaya <rokaiamagdi@gmail.com>
2021-07-13 13:30:57 +0200

    compose 2 query

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,40 +1,43 @@
 import React, { useState } from "react";
-import { graphql } from "@apollo/client/react/hoc";
-import {getAuthorsQuery } from '../Queries/queries'
+import { graphql, compose } from "@apollo/client/react/hoc";
+import { getAuthorsQuery, addBookMutation } from "../Queries/queries";
 const AddBook = (props) => {
-   const [name,setName]= useState("")
-   const [genre,setGenre]= useState("")
-   const [authorid,setAuthorid]= useState("")
-
+  const [name, setName] = useState("");
+  const [genre, setGenre] = useState("");
+  const [authorid, setAuthorid] = useState("");
 
   const displayAuthors = () => {
-      console.log(props);
+    console.log(props);
     let data = props.data;
     if (data.loading) {
       return <div>Loading Authors</div>;
     } else {
       return data.authors.map((author) => {
-        return <option key={author.id} value={author.id} >{author.name}</option>;
+        return (
+          <option key={author.id} value={author.id}>
+            {author.name}
+          </option>
+        );
       });
     }
   };
-  const submitForm =(e) => {
-      e.preventDefault();
-      console.log(name,genre,authorid);
-  }
+  const submitForm = (e) => {
+    e.preventDefault();
+    console.log(name, genre, authorid);
+  };
   return (
     <form id="add-book" onSubmit={submitForm}>
       <div className="field">
         <label>Book name:</label>
-        <input type="text" onChange={(e)=>setName(e.target.value)} />
+        <input type="text" onChange={(e) => setName(e.target.value)} />
       </div>
       <div className="field">
         <label>Genre:</label>
-        <input type="text" onChange={(e)=>setGenre(e.target.value)}/>
+        <input type="text" onChange={(e) => setGenre(e.target.value)} />
       </div>
       <div className="field">
         <label>Author:</label>
-        <select onChange={(e)=>setAuthorid(e.target.value)}>
+        <select onChange={(e) => setAuthorid(e.target.value)}>
           <option>Select author</option>
           {displayAuthors()}
         </select>
@@ -44,4 +47,7 @@ const AddBook = (props) => {
   );
 };
 
-export default  graphql(getAuthorsQuery)(AddBook);
+export default compose(
+  graphql(getAuthorsQuery, {name:"d"}),
+  graphql(addBookMutation , {name:"s"})
+)(AddBook);
